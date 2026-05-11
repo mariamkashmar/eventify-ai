86e42ff149974ac08a71b286d7b8760976099c5a
mariamkashmar <mariamkashmar19@gmail.com>
2026-05-11 22:01:59 +0300

    Fix Render import paths

--- Backend/app.js
+++ Backend/app.js
@@ -1,14 +1,18 @@
+const dns = require("dns");
+dns.setDefaultResultOrder("ipv4first");
+
 require("dotenv").config();
-const express = require('express'); // import express
-const cors = require('cors');
-const app = express();
+require("./Config/Config");
+
+const express = require("express");
+const cors = require("cors");
 const path = require("path");
 
-app.use(cors());
+const app = express();
 
-//middleware functions to run between backend and frontend
-app.use(express.json()); // can accept JSON data from request
-const db=require('./Config/Config');
+app.use(cors());
+app.use(express.json());
+
 const userRoute = require("./Routes/userRoute");
 const eventRoute = require("./Routes/eventRoute");
 const registrationRoute = require("./Routes/registrationRoute");
@@ -17,7 +21,6 @@ const startReminderService = require("./Services/reminderService");
 const inviteRoute = require("./Routes/inviteRoute");
 
 app.use("/uploads", express.static(path.join(__dirname, "uploads")));
-app.use("/api/ai", aiRoute);
 
 app.use("/api/users", userRoute);
 app.use("/api/events", eventRoute);
@@ -25,21 +28,13 @@ app.use("/api/registrations", registrationRoute);
 app.use("/api/ai", aiRoute);
 app.use("/api/invites", inviteRoute);
 
-
 app.get("/", (req, res) => {
   res.send("Eventify Backend is Running");
 });
-// start the server
-// server is listening to the requests /users /products
-/**   we need to decide the port 
-http://localhost:PORT/
-:3000
-:5000
-:3456
-*/
-const PORT = 5000;
-app.listen(PORT, ()=>{
-    console.log("Server is running on port ",PORT)
-    startReminderService();
-})
 
+const PORT = 5000;
+
+app.listen(PORT, () => {
+  console.log("Server is running on port", PORT);
+  startReminderService();
+});
