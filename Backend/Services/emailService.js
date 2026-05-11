cf7515d63e56a34d72fa4790b5a024f3db14eb7a
mariamkashmar <mariamkashmar19@gmail.com>
2026-05-11 23:48:38 +0300

    Configure Brevo SMTP

--- Backend/Services/emailService.js
+++ Backend/Services/emailService.js
@@ -1,15 +1,12 @@
 const nodemailer = require("nodemailer");
 
 const transporter = nodemailer.createTransport({
-  host: "smtp.gmail.com",
-  port: 587,
+  host: process.env.EMAIL_HOST,
+  port: process.env.EMAIL_PORT,
   secure: false,
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
-  },
-  tls: {
-    rejectUnauthorized: false,
   },
 });
 
