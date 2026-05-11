a5a49495b58f68367b6da381c755c27fc611b5c6
mariamkashmar <mariamkashmar19@gmail.com>
2026-05-11 23:59:43 +0300

    Fix Brevo SMTP

--- Backend/Services/emailService.js
+++ Backend/Services/emailService.js
@@ -2,7 +2,7 @@ const nodemailer = require("nodemailer");
 
 const transporter = nodemailer.createTransport({
   host: process.env.EMAIL_HOST,
-  port: process.env.EMAIL_PORT,
+  port: Number(process.env.EMAIL_PORT),
   secure: false,
   auth: {
     user: process.env.EMAIL_USER,
